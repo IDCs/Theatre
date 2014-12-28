5c28efcc32a855264b7315195be30c851608edc0
AyARL <ayamrau@gmail.com>
2014-12-28 04:36:03 +0000

    Classes for controlling the narrative flow

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Top level game control
+/// Issues events for UI to subscribe to
+/// </summary>
+public class GameController : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Narrative/NarrativeNode.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class NarrativeNode : MonoBehaviour
+{
+    [SerializeField]
+    private SubtitledAudio[] audioPlaylist; // All subtitled audio files to play in order
+    [SerializeField]
+    private NarrativeNode[] prerequisites; // All other nodes that need to be triggered before this one becomes active
+
+    // Was this node previosuly triggered
+    public bool Triggered { get; private set; }
+    // Called when this node triggers
+    public UnityAction OnNodeTriggered { get; set; }
+
+    // Has this node finished playing all the audio
+    public bool FinishedPlaying { get; private set; }
+    // Called when this node finished playing all audio
+    public UnityAction OnNodeFinishedPlaying { get; set; }
+
+    public void TriggerNode()
+    {
+        // If the node wasn't previously triggered, play the audio
+        if (!Triggered)
+        {
+            Triggered = true;
+            StartCoroutine(Play());
+        }
+    }
+
+    private IEnumerator Play()
+    {
+        foreach(SubtitledAudio audio in audioPlaylist)
+        {
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
