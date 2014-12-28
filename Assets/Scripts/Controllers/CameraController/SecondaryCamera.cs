4e95bb6130c23e2cbf3529263d8d81d731f8969a
AyARL <ayamrau@gmail.com>
2014-12-28 09:55:09 +0000

    Post-process cameras copy transform data from the main camera

---
+++ Assets/Scripts/Controllers/CameraController/SecondaryCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondaryCamera : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Camera.main.transform.position;
+        transform.rotation = Camera.main.transform.rotation;
+    }
+}
