0bb5e0a038248f5c84e2d9b45f0e59458315bc3f
AyARL <ayamrau@gmail.com>
2015-01-01 08:57:19 +0000

    Object node, subscribed to object events, but they don't appear to be called yet

---
+++ Assets/Scripts/Narrative/ObjectNode.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DeceptionNarrative
+{
+    [RequireComponent(typeof(NarrativeNode), typeof(CObject))]
+    public class ObjectNode : MonoBehaviour
+    {
+        // NarrativeNode triggered by this element
+        private NarrativeNode narrativeNode = null;
+
+        // CObject this node is observing
+        private CObject interactableObject = null;
+
+        private void Start()
+        {
+            narrativeNode = GetComponent<NarrativeNode>();
+            interactableObject = GetComponent<CObject>();
+
+            // subscribe to pick up and drag events of the object
+            interactableObject.OnObjectCollected += (item) => ObjectInteractionStarted();
+            interactableObject.OnObjectDragged += (go) => ObjectInteractionStarted();
+        }
+
+        private void ObjectInteractionStarted()
+        {
+            // Attempt to trigger the narrative node
+            narrativeNode.TriggerActivated();
+        }
+    }
+}
